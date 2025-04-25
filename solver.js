871b39dfe54902e388e8c5a385a8e1676c007e02
TJ-09 <13243354+TJ-09@users.noreply.github.com>
2025-04-25 19:14:34 +0100

    Update solver.js
    Updated to work April 2025 layout of minesweeper online.
    
    Updated logic for button addition to prevent multiple buttons appearing

--- solver.js
+++ solver.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         minesweeper solver
 // @namespace    http://tampermonkey.net/
-// @version      2023-12-27
+// @version      2025-04-25
 // @description  try to take over the world!
 // @author       You
 // @match        https://minesweeper.online/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=minesweeper.online
-// @grant        none
+// @grant        GM_log
 // ==/UserScript==
 
 (function() {
@@ -15,8 +15,8 @@
 })();
 
 function convert() {
-	// Find the div with id 'g64'
-    let div = document.getElementById('G64');
+    // Find the div with id 'g64'
+    let div = document.getElementById('AreaBlock');
 
     // Get all button elements within the div
     let buttons = div.getElementsByTagName('button');
@@ -42,62 +42,65 @@ function convert() {
     // Calculate the total value
     let mines = hundreds * 100 + tens * 10 + ones;
 
-   	let result = "";
-   	let rows = document.getElementsByClassName("clear").length
-   	var cells = document.getElementsByClassName("cell")
-	var columns = cells.length / rows
+    let result = "";
+    let rows = document.getElementsByClassName("clear").length
+    var cells = document.getElementsByClassName("cell")
+    var columns = cells.length / rows
 
-	for (var i = 0; i < cells.length; i++) {
-   		if(i % rows == 0 && i !== 0) {
-			result += ":"
-		}
-		if (cells[i].classList.contains('hd_closed')) {
-                    result += 'x';
-		}
-		if (cells[i].classList.contains('hd_flag')) {
-                    mines += 1;
-		}
-		if (cells[i].classList.contains('hd_type0')) {
-                    result += '.';
-		}
-		if (cells[i].classList.contains('hd_type1')) {
-                    result += '1';
-		}
-		if (cells[i].classList.contains('hd_type2')) {
-                    result += '2';
-		}
-		if (cells[i].classList.contains('hd_type3')) {
-                    result += '3';
-		}
-		if (cells[i].classList.contains('hd_type4')) {
-                    result += '4';
-		}
-		if (cells[i].classList.contains('hd_type5')) {
-		    result += '5';
-		}
-		if (cells[i].classList.contains('hd_type6')) {
-		    result += '6';
-		}
-		if (cells[i].classList.contains('hd_type7')) {
- 			result += '7';
-		}
-		if (cells[i].classList.contains('hd_type8')) {
-		    result += '8';
-		}
-	}
-    	var link = `https://mrgris.com/projects/minesweepr/demo/analyzer/?w=${columns}&h=${rows}&mines=${mines}&board=${result}`;
+    for (var i = 0; i < cells.length; i++) {
+        if(i % rows == 0 && i !== 0) {
+            result += ":"
+        }
+        if (cells[i].classList.contains('hdd_closed')) {
+            result += 'x';
+        }
+        if (cells[i].classList.contains('hdd_flag')) {
+            mines += 1;
+        }
+        if (cells[i].classList.contains('hdd_type0')) {
+            result += '.';
+        }
+        if (cells[i].classList.contains('hdd_type1')) {
+            result += '1';
+        }
+        if (cells[i].classList.contains('hdd_type2')) {
+            result += '2';
+        }
+        if (cells[i].classList.contains('hdd_type3')) {
+            result += '3';
+        }
+        if (cells[i].classList.contains('hdd_type4')) {
+            result += '4';
+        }
+        if (cells[i].classList.contains('hdd_type5')) {
+            result += '5';
+        }
+        if (cells[i].classList.contains('hdd_type6')) {
+            result += '6';
+        }
+        if (cells[i].classList.contains('hdd_type7')) {
+            result += '7';
+        }
+        if (cells[i].classList.contains('hdd_type8')) {
+            result += '8';
+        }
+    }
+    var link = `https://mrgris.com/projects/minesweepr/demo/analyzer/?w=${columns}&h=${rows}&mines=${mines}&board=${result}`;
 
 
-	let button = document.createElement('button');
-	button.innerHTML = 'Click me';
-	button.onclick = function() {
-  	  window.open(link, '_blank'); // Replace with your URL
-	};
-	let div2 = document.getElementById('G64');
-	div2.appendChild(button);
+    let button = document.createElement('button');
+    button.innerHTML = 'Click me';
+    button.onclick = function() {
+        window.open(link, '_blank'); // Replace with your URL
+    };
+    let div2 = document.getElementById('GameBottomPanelBlock');
+    if (div2.children.length === 4){
+       div2.appendChild(button);
+    }
+
 }
 
 function getNumberFromClassName(className) {
-    let match = className.match(/hd_top-area-num(\d)/);
+    let match = className.match(/hdd_top-area-num(\d)/);
     return match ? parseInt(match[1]) : 0;
 }
